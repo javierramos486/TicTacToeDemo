a30c4daf1495add5ca5214f119e3a404e0b5fc15
Francisco Javier Ramos <francisco.javier.ramos0010@gmail.com>
2018-03-10 16:47:11 -0800

    Added streaming assets download code. Added scrollview panel.

---
+++ TicTacToeDemo/Assets/Scripts/PlayerButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerButtonController : MonoBehaviour
+{
+    [SerializeField]
+    private Button playerOneButton;
+    [SerializeField]
+    private Button playerTwoButton;
+
+	// Use this for initialization
+	void Start () {
+        StreamingAssets.GetInstance().OnLoadFinish += Handle_OnLoadFinish;
+	}
+
+    private void OnDisable() {
+        StreamingAssets.GetInstance().OnLoadFinish -= Handle_OnLoadFinish;
+    }
+
+    void Handle_OnLoadFinish() {
+        Debug.Log("Wow! Finished loading");
+        Sprite[] playerOneSprites = StreamingAssets.GetInstance().GetPlayerOneSpriteArray();
+        Sprite[] playerTwoSprites = StreamingAssets.GetInstance().GetPlayerTwoSpritesArray();
+
+        if (playerOneSprites != null || playerOneSprites.Length != 0) {
+            playerOneButton.interactable = true;
+        }
+
+        if (playerTwoSprites != null || playerTwoSprites.Length != 0) {
+            playerTwoButton.interactable = true;
+        }
+    }
+}
---
+++ TicTacToeDemo/Assets/Scripts/StreamingAssets.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StreamingAssets : MonoBehaviour {
+
+    private static StreamingAssets instance;
+
+    [SerializeField]
+    private List<Sprite> playerOneSprites = new List<Sprite>();
+    [SerializeField]
+    private List<Sprite> playerTwoSprites = new List<Sprite>();
+
+    [HideInInspector]
+    public delegate void LoadingEventHandler();
+    [HideInInspector]
+    public event LoadingEventHandler OnLoadFinish;
+
+    private void Awake() {
+        if (instance == null) { instance = this; }
+        else { Destroy(gameObject); }
+    }
+
+    private void Start() {
+        DirectoryInfo directoryInfo = new DirectoryInfo(Application.streamingAssetsPath);
+        Debug.Log("Streaming Assets Path: " + Application.streamingAssetsPath);
+        FileInfo[] playerFiles = directoryInfo.GetFiles("*_player_*.png");
+
+        if (playerFiles.Length > 0) {
+            StartCoroutine(DownloadPlayerSprites(playerFiles));
+        }
+
+    }
+
+    private IEnumerator DownloadPlayerSprites(FileInfo[] playerFiles) {
+        foreach (FileInfo file in playerFiles) {
+            string spriteFileWithoutExtension = Path.GetFileNameWithoutExtension(file.Name);
+            string[] spriteNameData = spriteFileWithoutExtension.Split("_"[0]);
+
+            string wwwSpriteFile = "file://" + file.FullName;
+            WWW www = new WWW(wwwSpriteFile);
+            yield return www;
+
+            Sprite sprite = Sprite.Create(www.texture, new Rect(0, 0, 100, 100), new Vector2(0.5f, 0.5f));
+            sprite.name = spriteNameData[0].ToUpper();
+
+            if (file.Name.Contains("player_one")) {
+                playerOneSprites.Add(sprite);
+                Debug.Log("Full name = " + sprite.name + " position = " +playerOneSprites.Count);
+            }
+
+            if (file.Name.Contains("player_two")) {
+                playerTwoSprites.Add(sprite);
+                Debug.Log("Full name = " + sprite.name + " position = " + playerTwoSprites.Count);
+            }
+        }
+
+        Debug.Log("Done loading");
+        if (OnLoadFinish != null)
+            OnLoadFinish();
+    }
+
+    public static StreamingAssets GetInstance() { return instance; }
+
+    public Sprite[] GetPlayerOneSpriteArray() { return playerOneSprites.ToArray(); }
+
+    public Sprite[] GetPlayerTwoSpritesArray() { return playerTwoSprites.ToArray(); }
+
+}
